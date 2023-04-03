f8087a4adc75a2839cf84868cc43a751f5600605
Jason Zhou <jasonzhou0404@gmail.com>
2023-04-02 22:47:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -116,8 +116,8 @@ function App() {
       />
       <Routes>
         <Route path="/*" element={<AppRoutes />} />
-        <Route path="/log/*" element={<LogRoutes />} />
-        <Route path="/blogs/*" element={<BlogRoutes />} />
+        {/* <Route path="/log/*" element={<LogRoutes />} />
+        <Route path="/blogs/*" element={<BlogRoutes />} /> */}
       </Routes>
     </div>
   );
